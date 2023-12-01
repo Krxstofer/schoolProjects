bd38689a53f0e023e6880506da48955467c35c6f
Kristofer Norman <kjn2418@email.vccs.edu>
2023-12-01 12:55:10 -0500

    Add project files.

---
+++ LAB WORK 8.1 Arrays.cpp
@@ -0,0 +1,11 @@
+// LAB WORK 8.1 Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
