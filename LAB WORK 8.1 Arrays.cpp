2ad79f2d309f9de59ed8b537f90a1a8d1052e2c2
Kristofer Norman <kjn2418@email.vccs.edu>
2023-12-01 13:13:02 -0500

    Loops through an array, multiplies first 25 elements times itself, and multiplies last 25 elements times 3.

--- LAB WORK 8.1 Arrays.cpp
+++ LAB WORK 8.1 Arrays.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    // Declaring array of 50 components type double
+    double alpha[50];
+
+    // Initialize first 25 components equal to the square of index variable
+    for (int i = 0; i < 25; i++) {
+        alpha[i] = i * i;
+    }
+
+    // Last 25 components are equal to three times the index variable
+    for (int i = 25; i < 50; i++) {
+        alpha[i] = 3 * i;
+    }
+
+    // Print the array with 10 elements per line
+    for (int i = 0; i < 50; i++) {
+        if (i % 10 == 0) {
+            cout << endl;
+        }
+
+        cout << alpha[i] << " ";
+    }
+
 }
 
